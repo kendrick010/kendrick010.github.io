08c452d77167c5272000be7476536316fe50851a
kendrick010 <kendrick@Kendricks-MacBook-Air.local>
2023-09-22 12:13:46 -0700

    website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
+// Miliseconds to year
+var oneDay = 1000 * 60 * 60 * 24 * 365
+
+// Birthdate to present date
+var birthDate = new Date("May 7, 2002");
+
+function getLifeSpan() {
+  var presentDate = new Date();
+  var lifeSpan = Math.round(presentDate.getTime() - birthDate.getTime()) / (oneDay);
+
+  lifeSpan = lifeSpan.toFixed(9);
+  document.getElementById('lifespan').innerHTML = lifeSpan;
 }
+
+setInterval(getLifeSpan, 1);
